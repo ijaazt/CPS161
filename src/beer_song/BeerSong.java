a2821d567c7195ef817214836497ff0deb1f0f0b
I'jaaz Tello <mitello@wccnet.edu>
2018-01-30 18:12:07 -0500

    Second version of BeerSong.
    I made the function less dependent on switches while also making it work
    with numbers between 10 and 20.
    
    The actual code is pretty spaghetti.
    
    I spend some of my time experimenting with different class/method
    attributes.
    
    I plan to do a final refactor.

--- src/beer_song/BeerSong.java
+++ src/beer_song/BeerSong.java
@@ -2,6 +2,7 @@ package beer_song;
 
 public class BeerSong {
 	private int initialAmount;
+
 	public BeerSong(int initialAmount) {
 		setInitialAmount(initialAmount);
 	}
@@ -12,55 +13,49 @@ public class BeerSong {
 		if (initialAmount > 99) initialAmount = 99;
 	}
 	
-	private String transformNumbers(int number) {
+	private int onesPlace(int number) {
+		if(tensPlace(number) > 0)
+			return number - (tensPlace(number) * 10);
+		return number;
+	}
+	private int tensPlace(int number) {
+		return number / 10;
+	}
 
-		int ones = number % 10;
-		int tens = number / 10;
-		String oneString = "";
-		String tenString = "";
-		switch(ones) {
-		case 0: oneString = "Zero"; break;
-		case 1: oneString = "One"; break;
-		case 2: oneString = "Two"; break;
-		case 3: oneString = "Three"; break;
-		case 4: oneString = "Four"; break;
-		case 5: oneString = "Five"; break;
-		case 6: oneString = "Six"; break;
-		case 7: oneString = "Seven"; break;
-		case 8: oneString = "Eight"; break;
-		case 9: oneString = "Nine"; break;
-		case 10:oneString = "Ten"; break;
-		}
-		
-		switch(tens) {
-		case 0: tenString = ""; break;
-		case 1: tenString = "Teen"; break;
-		case 2: tenString = "Twenty"; break;
-		case 3: tenString = "Thirty"; break;
-		case 4: tenString = "Fourty"; break;
-		case 5: tenString = "Fifty"; break;
-		case 6: tenString = "Sixty"; break;
-		case 7: tenString = "Seventy"; break;
-		case 8: tenString = "Eighty"; break;
-		case 9: tenString = "Ninety"; break;
-		}
-		
-		
-		
-		if(ones == number)
-			return oneString;
-		
-		if((number % 10) == 0)
-			return tenString;
-		if((number / 10) == 1)
-			return oneString + "-" + tenString;
-		
-		return tenString + "-" + oneString;
+	private String returnStringNum (int ones) {
+		String[] numberEquivalents = {"Zero", "One", "Two", "Three", "Four", "Five", "Six",
+				"Seven", "Eight", "Nine"};
+		return numberEquivalents[ones];
+	}
+	private String tensAndUp (int twenties) {
+		String[] numberEquivalents = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
+				"Seventy", "Eighty", "Ninety"};
+		return numberEquivalents[twenties];
+	}
+	private String tensAndUp (int tens, boolean tensAreIt) {
+		String[] numberEquivalents = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+				"Seventeen", "Eighteen", "Nineteen"};
+		return (tensAreIt) ? numberEquivalents[tens] : tensAndUp(tens);
 	}
 	
+	private String returnStringNum (int tens, int ones) {
+		char delimiter = '-';
+		if (tens == 0)
+			return returnStringNum(ones);
+		if ((tens == 1) && (ones >= 0))
+			return tensAndUp(ones, true);
+		if((ones == 0) && (tens != 0))
+			return tensAndUp(tens);
+		
+		return  tensAndUp(tens) + delimiter + returnStringNum(ones);
+	}
+
+	private String intConverter(int number) {
+		return returnStringNum(tensPlace(number), onesPlace(number));
+	}
 	public void printSong() {
 		for(int i = initialAmount; i > 0; i--) {
-			String iTransformed = transformNumbers(i);
+			String iTransformed = intConverter(i);
 			System.out.printf("%s bottles on the wall,", iTransformed);
 			System.out.println();
 			System.out.printf("%s bottles on the wall,", iTransformed);
@@ -69,7 +64,7 @@ public class BeerSong {
 			System.out.printf("%s bottles of beer the wall.", iTransformed);
 			System.out.println();
 		}
-		
+		System.out.println("Zero bottles of beer on the wall.");
 	}
 	
 	
